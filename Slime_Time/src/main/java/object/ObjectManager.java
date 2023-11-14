47d909dca33546da3057e9296cf16292034e4ee1
Dawson Le <97484417+kledawson@users.noreply.github.com>
2023-11-13 22:01:43 -0800

    Added Rock entity!

--- Slime_Time/src/main/java/object/ObjectManager.java
+++ Slime_Time/src/main/java/object/ObjectManager.java
@@ -2,6 +2,7 @@ package object;
 
 import main.GameApplication;
 import object.*;
+import tiles_interactive.Rock;
 
 public class ObjectManager {
 
@@ -13,8 +14,30 @@ public class ObjectManager {
 
     // Loads all Object Positions
     public void setObject() {
+
         ga.obj[0] = new Example_Object(ga);
         ga.obj[0].worldX = 0 * ga.TILE_SIZE;
         ga.obj[0].worldY = 0 * ga.TILE_SIZE;
+
+        ga.rock[0] = new Rock(ga);
+        ga.rock[0].worldX = 25 * ga.TILE_SIZE;
+        ga.rock[0].worldY = 23 * ga.TILE_SIZE;
+
+        ga.rock[1] = new Rock(ga);
+        ga.rock[1].worldX = 24 * ga.TILE_SIZE;
+        ga.rock[1].worldY = 23 * ga.TILE_SIZE;
+
+        ga.rock[2] = new Rock(ga);
+        ga.rock[2].worldX = 23 * ga.TILE_SIZE;
+        ga.rock[2].worldY = 23 * ga.TILE_SIZE;
+
+        ga.rock[3] = new Rock(ga);
+        ga.rock[3].worldX = 22 * ga.TILE_SIZE;
+        ga.rock[3].worldY = 23 * ga.TILE_SIZE;
+
+        ga.rock[4] = new Rock(ga);
+        ga.rock[4].worldX = 21 * ga.TILE_SIZE;
+        ga.rock[4].worldY = 23 * ga.TILE_SIZE;
+
     }
 }
