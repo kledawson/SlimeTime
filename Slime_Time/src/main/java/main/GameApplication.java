2a5a2cc19093e2067bb90787b97f3492cabc0f9b
Johnny Nguyen <jqbnguyen80@gmail.com>
2023-11-28 21:13:09 -0800

    Work in progress upgrade menu and code quality changes

--- Slime_Time/src/main/java/main/GameApplication.java
+++ Slime_Time/src/main/java/main/GameApplication.java
@@ -3,11 +3,13 @@ package main;
 import entity.Entity;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
@@ -21,6 +23,8 @@ import object.SuperObject;
 import tile.TileManager;
 import tiles_interactive.Rock;
 
+import java.io.FileInputStream;
+
 
 // Game Application
 public class GameApplication extends Application {
@@ -28,24 +32,24 @@ public class GameApplication extends Application {
     final int ORIGINAL_TILE_SIZE = 16; // 16x16 tile
     public final int SCALE = 3;
     public final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; // 48x48 tile
-    public final int MAX_SCREEN_COL_SMALL = 20;
-    public final int MAX_SCREEN_ROW_SMALL = 12;
+    public final int MAX_SCREEN_COL_SMALL = 24;
+    public final int MAX_SCREEN_ROW_SMALL = 16;
     public final int MAX_SCREEN_COL_LARGE = 54;
     public final int MAX_SCREEN_ROW_LARGE = 29;
 
     // Current maximum screen col and row values
-    public int MAX_SCREEN_COL = MAX_SCREEN_COL_LARGE;
-    public int MAX_SCREEN_ROW = MAX_SCREEN_ROW_LARGE;
+    public int MAX_SCREEN_COL = MAX_SCREEN_COL_SMALL;
+    public int MAX_SCREEN_ROW = MAX_SCREEN_ROW_SMALL;
 
-    public final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 768 px
-    public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 576 px
+    public final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 1152 px
+    public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 768 px
     public final int MAX_WORLD_COL = 100;
     public final int MAX_WORLD_ROW = 100;
     int FPS = 60;
 
     //for full screen UI
-    int SCREEN_WIDTH2 = TILE_SIZE * MAX_SCREEN_COL_SMALL;
-    int SCREEN_HEIGHT2 = TILE_SIZE * MAX_SCREEN_ROW_SMALL;
+    int SCREEN_WIDTH2 = TILE_SIZE * MAX_SCREEN_COL_LARGE;
+    int SCREEN_HEIGHT2 = TILE_SIZE * MAX_SCREEN_ROW_LARGE;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler(this, false);
@@ -65,6 +69,16 @@ public class GameApplication extends Application {
 
     public double mouseX;
     public double mouseY;
+
+//    Button upgradeMeleeButton; // Declare the buttons
+//    Button upgradeArmorButton;
+//    Button upgradeProjectileButton;
+//    Button upgradeBootsButton;
+
+    ImageView upgradeBootsButton;
+    ImageView upgradeMeleeButton;
+    ImageView upgradeArmorButton;
+    ImageView upgradeProjectileButton;
 
     @Override
     public void start(Stage stage) {
@@ -93,21 +107,74 @@ public class GameApplication extends Application {
         });
 
         Button showUpgradeScreenButton = new Button("Upgrade");
-        showUpgradeScreenButton.setOnAction(e -> {
-            if (!upgradeButtonsVisible) {
-                ui.renderUpgradeScreen();
-                upgradeButtonsVisible = true;
-            } else {
-                ui.removeUpgradeButtons();
-                upgradeButtonsVisible = false;
-            }
-        });
         showUpgradeScreenButton.setLayoutX(SCREEN_WIDTH - 100);
         showUpgradeScreenButton.setLayoutY(20);
 
 
         // Add the button to the root pane
         root.getChildren().add(showUpgradeScreenButton);
+
+
+        // Set button positions and sizes
+        int buttonWidth = 144;
+        int buttonHeight = 72;
+        int buttonSpacing = 64; // Spacing between buttons
+        int totalButtonWidth = 4 * buttonWidth + 3 * buttonSpacing;
+        int buttonX = SCREEN_WIDTH / 8 + TILE_SIZE;
+        int buttonY = SCREEN_HEIGHT * 5 / 8;
+
+        try {
+            Image buttonImage = new Image(new FileInputStream("Slime_Time/res/ui/button_image.png"), buttonWidth, buttonHeight, false, false);
+            upgradeBootsButton = new ImageView(buttonImage);
+            upgradeMeleeButton = new ImageView(buttonImage);
+            upgradeArmorButton = new ImageView(buttonImage);
+            upgradeProjectileButton = new ImageView(buttonImage);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        upgradeBootsButton.setLayoutX(buttonX);
+        upgradeBootsButton.setLayoutY(buttonY);
+
+        upgradeMeleeButton.setLayoutX(buttonX + buttonWidth + buttonSpacing);
+        upgradeMeleeButton.setLayoutY(buttonY);
+
+        upgradeArmorButton.setLayoutX(buttonX + 2 * (buttonWidth + buttonSpacing));
+        upgradeArmorButton.setLayoutY(buttonY);
+
+        upgradeProjectileButton.setLayoutX(buttonX + 3 * (buttonWidth + buttonSpacing));
+        upgradeProjectileButton.setLayoutY(buttonY);
+
+
+        upgradeBootsButton.setVisible(false);
+        upgradeMeleeButton.setVisible(false);
+        upgradeArmorButton.setVisible(false);
+        upgradeProjectileButton.setVisible(false);
+
+        // Add buttons to the root pane
+        root.getChildren().addAll(
+                upgradeBootsButton,
+                upgradeMeleeButton,
+                upgradeArmorButton,
+                upgradeProjectileButton
+        );
+
+        showUpgradeScreenButton.setOnAction(e -> {
+            if (upgradeBootsButton.isVisible()) {
+                upgradeBootsButton.setVisible(false);
+                upgradeMeleeButton.setVisible(false);
+                upgradeArmorButton.setVisible(false);
+                upgradeProjectileButton.setVisible(false);
+            }
+            else {
+                upgradeBootsButton.setVisible(true);
+                upgradeMeleeButton.setVisible(true);
+                upgradeArmorButton.setVisible(true);
+                upgradeProjectileButton.setVisible(true);
+            }
+        });
+
         stage.setTitle("2D Adventure");
         stage.setScene(scene);
         stage.show();
